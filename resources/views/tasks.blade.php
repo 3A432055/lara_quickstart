77cbc5dbea03f9b84bcd1009dccd7f5c94f9e59f
CHEN <david860120@gmail.com>
2019-10-18 20:09:25 +0800

    no message

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,4 +1,5 @@
-<?php
+// resources/views/tasks.blade.php
+
 @extends('layouts.app')
 
 @section('content')
@@ -35,50 +36,25 @@
 
     <!-- 代辦：目前任務 -->
     @if (count($tasks) > 0)
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                目前任務
-            </div>
+        <tbody>
+        @foreach ($tasks as $task)
+            <tr>
+                <!-- 任務名稱 -->
+                <td class="table-text">
+                    <div>{{ $task->name }} </div>
+                </td>
+                <td>
+                    <!-- 代辦：刪除按鈕 -->
+                    <form action="/task/{{ $task->id }}" method="POST">
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE') }}
+                        <button>刪除任務</button>
+                    </form>
+                </td>
 
-            <div class="panel-body">
-                <table class="table table-striped task-table">
-
-                    <!-- 表頭 -->
-                    <thead>
-                    <th>Task</th>
-                    <th>&nbsp;</th>
-                    </thead>
-
-                    <!-- 表身 -->
-                    <tbody>
-                    @foreach ($tasks as $task)
-                        <tr>
-                            <!-- 任務名稱 -->
-                            <td class="table-text">
-                                <div>{{ $task->name }}</div>
-                            </td>
-
-                            <td>
-                                <!-- 代辦：刪除按鈕 -->
-                            </td>
-                            <form action="/task/{{ $task->id }}" method="POST">
-                                {{ csrf_field() }}
-                                {{ method_field('DELETE') }}
-
-                                <button>刪除任務</button>
-                                Route::delete('/task/{task}', function (Task $task) {
-                                $task->delete();
-
-                                return redirect('/');
-                                });
-                            </form>
-                            </td>
-
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
+            </tr>
+        @endforeach
+        </tbody>
     @endif
+
 @endsection
